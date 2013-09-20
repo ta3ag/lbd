5e2bd149d97edb8d73f1ff1f0623883726a4105b
Jonathan <gamma_chen@yahoo.com.tw>
2013-09-20 16:46:22 +0800

    Add Chapter Other data type

---
+++ lib/Target/Cpu0/LLVMBackendTutorialExampleCode/InputFiles/ch9_4.cpp
@@ -0,0 +1,28 @@
+// clang -I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk/usr/include/ -c ch8_4.cpp -emit-llvm -o ch8_4.bc
+// /Users/Jonathan/llvm/test/cmake_debug_build/bin/Debug/llc -march=cpu0 -relocation-model=pic -filetype=asm ch8_4.bc -o ch8_4.cpu0.s
+
+/// start
+#include <alloca.h>
+
+int sum(int x1, int x2, int x3, int x4, int x5, int x6)
+{
+  int sum = x1 + x2 + x3 + x4 + x5 + x6;
+  
+  return sum; 
+}
+
+int weight_sum(int x1, int x2, int x3, int x4, int x5, int x6)
+{
+  int *b = (int*)alloca(sizeof(int) * x1);
+  *b = 1111;
+  int weight = sum(6*x1, x2, x3, x4, 2*x5, x6);
+  
+  return weight; 
+}
+
+int main()
+{
+  int a = weight_sum(1, 2, 3, 4, 5, 6);
+  
+  return a;
+}
---
+++ lib/Target/Cpu0/LLVMBackendTutorialExampleCode/InputFiles/ch_run_sum_i.cpp
@@ -0,0 +1,52 @@
+// ~/llvm/release/cmake_debug_build/bin/Debug/clang -target mips-unknown-linux-gnu -c ch_run_sum_i.cpp -emit-llvm -o ch_run_sum_i.bc
+// /Users/Jonathan/llvm/test/cmake_debug_build/bin/Debug/llc -march=cpu0 -relocation-model=static -filetype=obj ch_run_sum_i.bc -o ch_run_sum_i.cpu0.o
+// /Users/Jonathan/llvm/test/cmake_debug_build/bin/Debug/llvm-objdump -d ch_run_sum_i.cpu0.o | tail -n +6| awk '{print "/* " $1 " */\t" $2 " " $3 " " $4 " " $5 "\t/* " $6"\t" $7" " $8" " $9" " $10 "\t*/"}' > ../cpu0_verilog/raw/cpu0s.hex
+
+// /Users/Jonathan/llvm/test/cmake_debug_build/bin/Debug/llvm-objdump -d ch_run_sum_i.cpu0.o | tail -n +6| awk '{print "/* " $1 " */\t" $2 " " $3 " " $4 " " $5 "\t/* " $6"\t" $7" " $8" " $9" " $10 "\t*/"}' > ../cpu0_verilog/redesign/cpu0s.hex
+
+// /home/cschen/test/lld_20130816/cmake_debug_build/bin/clang -target mips-unknown-linux-gnu -c ch_run_sum_i.cpp -emit-llvm -o ch_run_sum_i.bc
+// /home/cschen/test/lld_20130816/cmake_debug_build/bin/llc -march=cpu0 -relocation-model=static -filetype=obj ch_run_sum_i.bc -o ch_run_sum_i.cpu0.o
+// /home/cschen/test/lld_20130816/cmake_debug_build/bin/lld -flavor gnu -target cpu0-unknown-linux-gnu ch_run_sum_i.cpu0.o -o a.out
+// /home/cschen/test/lld_20130816/cmake_debug_build/bin/llvm-objdump -elf2hex a.out > ../cpu0_verilog/raw/cpu0s.hex
+
+// /home/Gamma/test/lld_0816/cmake_debug_build/bin/clang -target mips-unknown-linux-gnu -c ch_run_sum_i.cpp -emit-llvm -o ch_run_sum_i.bc
+// /home/Gamma/test/lld_0816/cmake_debug_build/bin/llc -march=cpu0 -relocation-model=static -filetype=obj ch_run_sum_i.bc -o ch_run_sum_i.cpu0.o
+// /home/Gamma/test/lld_0816/cmake_debug_build/bin/lld -flavor gnu -target cpu0-unknown-linux-gnu ch_run_sum_i.cpu0.o -o a.out
+// /home/Gamma/test/lld_0816/cmake_debug_build/bin/llvm-objdump -elf2hex a.out > ../cpu0_verilog/raw/cpu0s.hex
+
+/// start
+#include "boot.cpp"
+
+#include "print.h"
+
+int sum_i(int amount, ...);
+
+int main()
+{
+  int a = 0;
+  a = sum_i(6, 0, 1, 2, 3, 4, 5);
+  print_integer(a);    // a = 15
+
+  return a;
+}
+
+#include "print.cpp"
+
+#include <stdarg.h>
+int sum_i(int amount, ...)
+{
+  int i = 0;
+  int val = 0;
+  int sum = 0;
+	
+  va_list vl;
+  va_start(vl, amount);
+  for (i = 0; i < amount; i++)
+  {
+    val = va_arg(vl, int);
+    sum += val;
+  }
+  va_end(vl);
+  
+  return sum; 
+}
