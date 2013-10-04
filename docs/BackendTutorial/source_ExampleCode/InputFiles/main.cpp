b33e30f31ebc50b61d5f9c358777f000f01596b7
Gamma Chen <cschen@marvell.com>
2013-10-04 23:30:36 +0800

    add commment in examplecode main.cpp

--- docs/BackendTutorial/source_ExampleCode/InputFiles/main.cpp
+++ docs/BackendTutorial/source_ExampleCode/InputFiles/main.cpp
@@ -1,3 +1,7 @@
+// gcc/clang -c -fPIC foobar.cpp
+// gcc/clang -shared -o libfoobar.so foobar.o
+// /home/Gamma/test/lld/cmake_debug_build/bin/lld -flavor gnu -target x86_64-unknown-linux-gnu -shared -o libfoobar.so foobar.o
+// gcc/clang main.cpp libfoobar.so
 
 /// start
 extern int foo(int x1, int x2);
