b5de61762a3cfab0e83f1bbb59f4ca679833d460
cschen <cschen@marvell.com>
2013-10-28 14:28:42 +0800

    lld

--- docs/BackendTutorial/source_ExampleCode/InputFiles/foobar.cpp
+++ docs/BackendTutorial/source_ExampleCode/InputFiles/foobar.cpp
@@ -3,6 +3,13 @@
 
 #include "dynamic_linker.h"
 
+int la(int x1, int x2)
+{
+  int sum = x1 + x1 + x2;
+  
+  return sum; 
+}
+
 int foo(int x1, int x2)
 {
   int sum = x1 + x2;
@@ -15,6 +22,7 @@ int bar()
   int a;
 //  ENABLE_TRACE;
   a = foo(2, 2);
+  a += la(2, 3); // 4+7=11
 
   return a;
 }
