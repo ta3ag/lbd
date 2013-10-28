c86e30918fac545f2afacc50845cfb4931447839
cschen <gamma_chen@yahoo.com.tw>
2013-10-28 14:20:46 +0800

    lld

--- docs/BackendTutorial/source_ExampleCode/InputFiles/build-printf-stdarg-2.sh
+++ docs/BackendTutorial/source_ExampleCode/InputFiles/build-printf-stdarg-2.sh
@@ -2,6 +2,8 @@
 #TOOLDIR=/home/Gamma/test/lld/cmake_debug_build/bin
 TOOLDIR=/home/cschen/test/lld/cmake_debug_build/bin
 
+/usr/local/llvm/release/cmake_debug_build/bin/clang -target mips-unknown-linux-gnu -c start.cpp -emit-llvm -o start.bc
+/usr/local/llvm/release/cmake_debug_build/bin/llvm-dis start.bc -o start.ll
 /usr/local/llvm/release/cmake_debug_build/bin/clang -target mips-unknown-linux-gnu -c printf-stdarg.c -emit-llvm -o printf-stdarg.bc
 /usr/local/llvm/release/cmake_debug_build/bin/clang -target mips-unknown-linux-gnu -c printf-stdarg-2.c -emit-llvm -o printf-stdarg-2.bc
 ${TOOLDIR}/llc -march=cpu0 -relocation-model=static -filetype=obj start.ll -o start.cpu0.o
