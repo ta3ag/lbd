36f95f0d9f56ab852564e7af12334b44579eb47f
cschen <cschen@marvell.com>
2013-10-28 16:01:23 +0800

    lld

--- docs/BackendTutorial/source_ExampleCode/InputFiles/build-dlinker.sh
+++ docs/BackendTutorial/source_ExampleCode/InputFiles/build-dlinker.sh
@@ -12,9 +12,9 @@ ${TOOLDIR}/llc -march=cpu0 -relocation-model=static -filetype=obj -cpu0-reserve-
 ${TOOLDIR}/llc -march=cpu0 -relocation-model=pic -filetype=obj -cpu0-reserve-gp=true -cpu0-no-cpload=true foobar.cpu0.bc -o foobar.cpu0.o
 ${TOOLDIR}/lld -flavor gnu -target cpu0-unknown-linux-gnu -shared -o libfoobar.cpu0.so foobar.cpu0.o
 ${TOOLDIR}/llc -march=cpu0 -relocation-model=static -filetype=obj -cpu0-reserve-gp=true start.ll -o start.cpu0.o
-/usr/local/llvm/release/cmake_debug_build/bin/clang -target mips-unknown-linux-gnu -c main.cpp -emit-llvm -o main.cpu0.bc
-${TOOLDIR}/llc -march=cpu0 -relocation-model=static -filetype=obj -cpu0-reserve-gp=true main.cpu0.bc -o main.cpu0.o
-${TOOLDIR}/lld -flavor gnu -target cpu0-unknown-linux-gnu start.cpu0.o printf-stdarg.cpu0.o dynamic_linker.cpu0.o main.cpu0.o libfoobar.cpu0.so
+/usr/local/llvm/release/cmake_debug_build/bin/clang -target mips-unknown-linux-gnu -c ch_dynamiclinker.cpp -emit-llvm -o ch_dynamiclinker.cpu0.bc
+${TOOLDIR}/llc -march=cpu0 -relocation-model=static -filetype=obj -cpu0-reserve-gp=true ch_dynamiclinker.cpu0.bc -o ch_dynamiclinker.cpu0.o
+${TOOLDIR}/lld -flavor gnu -target cpu0-unknown-linux-gnu start.cpu0.o printf-stdarg.cpu0.o dynamic_linker.cpu0.o ch_dynamiclinker.cpu0.o libfoobar.cpu0.so
 ${TOOLDIR}/llvm-objdump -elf2hex -dumpso libfoobar.cpu0.so > ../cpu0_verilog/raw/libso.hex
 ${TOOLDIR}/llvm-objdump -elf2hex a.out > ../cpu0_verilog/raw/cpu0s.hex
 cp dynstr dynsym so_func_offset global_offset ../cpu0_verilog/raw/.
