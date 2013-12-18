c21a1aa0cfe0a3c311bbfdb57ceb4ede3ae92de6
cschen <cschen@marvell.com>
2013-12-18 13:46:07 +0800

    Correct typing error

--- docs/BackendTutorial/source_ExampleCode/InputFiles/ch8_1_4.cpp
+++ docs/BackendTutorial/source_ExampleCode/InputFiles/ch8_1_4.cpp
@@ -1,5 +1,5 @@
-// clang -target mips-unknown-linux-gnu -c ch8_1_5.cpp -emit-llvm -o ch8_1_5.bc
-// /Users/Jonathan/llvm/test/cmake_debug_build/bin/Debug/llc -march=cpu0 -relocation-model=pic -filetype=asm ch8_1_5.bc -o -
+// clang -target mips-unknown-linux-gnu -c ch8_1_4.cpp -emit-llvm -o ch8_1_4.bc
+// /Users/Jonathan/llvm/test/cmake_debug_build/bin/Debug/llc -march=cpu0 -relocation-model=pic -filetype=asm ch8_1_4.bc -o -
 
 /// start
 int main()
