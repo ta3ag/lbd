6ab27276103077d2a467e80afc9aee86f870489e
cschen <cschen@marvell.com>
2013-12-20 12:29:23 +0800

    Adjust Example Code

--- docs/BackendTutorial/source_ExampleCode/InputFiles/foobar.cpp
+++ docs/BackendTutorial/source_ExampleCode/InputFiles/foobar.cpp
@@ -18,7 +18,7 @@ int foo(int x1, int x2)
 }
 
 #if 0
-int power(int x)
+int factorial(int x)
 {
   if (x > 0)
     return x*power(x-1);
@@ -33,7 +33,7 @@ int bar()
 //  ENABLE_TRACE;
   a = foo(2, 2);
   a += la(2, 3); // 4+7=11
-//  a += power(4); // 11+24=35
+//  a += factorial(4); // 11+24=35
 
   return a;
 }
