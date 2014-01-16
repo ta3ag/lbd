98cabdd163f9d29743141cc7de87375f3dd4b46e
Jonathan <gamma_chen@yahoo.com.tw>
2014-01-16 23:09:17 +0800

    inlineasm

--- docs/BackendTutorial/source_ExampleCode/InputFiles/ch_lld_staticlink.cpp
+++ docs/BackendTutorial/source_ExampleCode/InputFiles/ch_lld_staticlink.cpp
@@ -35,8 +35,8 @@ int test_staticlink()
   else
     printf(", FAIL\n");
   a = test_inlineasm();
-  printf("test_inlineasm() = %d", a); // a = 37
-  if (a == 37)
+  printf("test_inlineasm() = %d", a); // a = 42
+  if (a == 42)
     printf(", PASS\n");
   else
     printf(", FAIL\n");
