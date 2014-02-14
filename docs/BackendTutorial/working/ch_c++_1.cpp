49765a718782818abad2e14bf9c3da7f8ccb018f
cschen <cschen@marvell.com>
2014-02-14 13:07:27 +0800

    c++ working

--- docs/BackendTutorial/working/ch_c++_1.cpp
+++ docs/BackendTutorial/working/ch_c++_1.cpp
@@ -4,14 +4,17 @@
 #include <iostream>
 using namespace std;
 
+extern "C" int printf(const char *format, ...);
+extern "C" int sprintf(char *out, const char *format, ...);
+
 class CPolygon {
   protected:
     int width, height;
   public:
     void set_values (int a, int b)
     { width=a; height=b; }
-    virtual int area (void) =0;
-//    virtual int area (void) { return 0;};
+//    virtual int area (void) =0;
+    virtual int area (void) { return 0;};
     void printarea (void)
 //    { printf("%d\n", this->area()); }
     { cout << this->area() << endl; }
@@ -29,14 +32,32 @@ class CTriangle: public CPolygon {
     { return (width * height / 2); }
 };
 
-int main () {
+class CAngle1: public CPolygon {
+  public:
+    int area (void)
+    { return (width * height / 4); }
+};
+
+int test_cpp_cout() {
   CPolygon * ppoly1 = new CRectangle;
   CPolygon * ppoly2 = new CTriangle;
+//  CPolygon * appoly3 = new CXangle;
+  CPolygon * appoly3 = new CAngle1;
   ppoly1->set_values (4,5);
   ppoly2->set_values (4,5);
+//  ppoly3->set_values (4,5);
   ppoly1->printarea();
   ppoly2->printarea();
+//  ppoly3->printarea();
   delete ppoly1;
   delete ppoly2;
+//  delete ppoly3;
   return 0;
 }
+
+#ifdef TEST_RUN
+int main() {
+  return test_cpp_cout();
+}
+#endif
+
