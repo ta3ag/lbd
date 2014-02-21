c171de4d92e76415720d08fe2fc1da1ff318341c
cschen <cschen@marvell.com>
2014-02-21 18:19:47 +0800

    working c++

---
+++ docs/BackendTutorial/working/rminput.sh
@@ -0,0 +1,8 @@
+PWD=`pwd`
+pushd ${PWD}
+rm -f dynstr dynsym dynstrAscii global_offset plt_offset so_func_offset libso.hex cpu0.hex *~ num_dyn_entry libfoobar.cpu0.so
+cd ../cpu0_verilog
+rm -f dynstr dynsym dynstrAscii global_offset plt_offset so_func_offset libso.hex cpu0.hex *~
+popd
+
+
