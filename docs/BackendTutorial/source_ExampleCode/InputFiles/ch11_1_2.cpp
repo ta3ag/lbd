e97f0bdc5af11344d680b3461a74d55afe554e06
Jonathan <gamma_chen@yahoo.com.tw>
2014-03-04 23:20:46 +0800

    AsmParser

--- docs/BackendTutorial/source_ExampleCode/InputFiles/ch11_1_2.cpp
+++ docs/BackendTutorial/source_ExampleCode/InputFiles/ch11_1_2.cpp
@@ -1,44 +1,8 @@
 // clang -target mips-unknown-linux-gnu -c ch11_1_2.cpp -emit-llvm -o ch11_1_2.bc
-// /Users/Jonathan/llvm/test/cmake_debug_build/bin/Debug/llc -march=cpu0 -relocation-model=pic -filetype=obj ch11_1_2.bc -o ch11_1_2.cpu0.o
+// /Users/Jonathan/llvm/test/cmake_debug_build/bin/Debug/llc -march=cpu0 -mcpu=cpu032II -relocation-model=pic -filetype=obj ch11_1_2.bc -o ch11_1_2.cpu0.o
 
 /// start
-#if 0
-asm("ld	$2, 8($sp)");
-asm("st	$0, 4($sp)");
-asm("addiu $3,	$ZERO, 0");
-asm("add $3, $1, $2");
-asm("sub $3, $2, $3");
-asm("mul $2, $1, $3");
-asm("div $3, $2");
-asm("divu $2, $3");
-asm("and $2, $1, $3");
-asm("or $3, $1, $2");
-asm("xor $1, $2, $3");
-asm("mult $4, $3");
-asm("multu $3, $2");
-asm("mfhi $3");
-asm("mflo $2");
-asm("mthi $2");
-asm("mtlo $2");
-asm("sra $2, $2, 2");
-asm("rol $2, $1, 3");
-asm("ror $3, $3, 4");
-asm("shl $2, $2, 2");
-asm("shr $2, $3, 5");
-asm("cmp $sw, $2, $3");
-asm("jeq $sw, 20");
-asm("jne $sw, 16");
-asm("jlt $sw, -20");
-asm("jle $sw, -16");
-asm("jgt $sw, -4");
-asm("jge $sw, -12");
-asm("swi 0x00000400");
-asm("jsub 0x000010000");
-asm("ret $lr");
-asm("jalr $t9");
-asm("li $3, 0x00700000");
-asm("la $3, 0x00800000($6)");
-asm("la $3, 0x00900000");
-#endif
 asm("slt $2, $2, $3");
 asm("beq $2, $3, 20");
+asm("ori $sw, $sw, 0x0020");
+asm("andi $sw, $sw, 0xffdf");
