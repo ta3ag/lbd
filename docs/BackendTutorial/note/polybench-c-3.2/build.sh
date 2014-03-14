90281e70b6ccb51a5f3872725aead01e9a72b429
cschen <cschen@marvell.com>
2014-03-14 11:36:51 +0800

    polly

--- docs/BackendTutorial/note/polybench-c-3.2/build.sh
+++ docs/BackendTutorial/note/polybench-c-3.2/build.sh
@@ -1,77 +1,100 @@
-#!/bin/sh -a
+#!/bin/bash -a
 
+rm -rf polybench
+mkdir polybench
+
+BUILD_ARM=0
+#BUILD_ARM=1
+
+#DUMP_RESULT=0
+DUMP_RESULT=1
+
+if [ ${BUILD_ARM} == 1 ]; then
+CPU="-mcpu=cortex-a9 -mfloat-abi=hard -mfpu=neon"
+LLVM_INSTALL=~/test/polly/llvm_arm_build
+else
+CPU=""
 LLVM_INSTALL=~/test/polly/llvm_build
-alias pollycc="${LLVM_INSTALL}/bin/clang -Xclang -load -Xclang ${LLVM_INSTALL}/lib/LLVMPolly.so"
+fi
 
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c datamining/covariance/covariance.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o covariance
+if [ ${DUMP_RESULT} == 1 ]; then
+echo "DUMP_RESULT == 1"
+CFLAG="-O3 -I utilities -I datamining/covariance utilities/polybench.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB ${CPU}"
+PCFLAG="-mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB ${CPU}"
+else
+CFLAG="-O3 -I utilities -I datamining/covariance utilities/polybench.c -DPOLYBENCH_TIME -DPOLYBENCH_USE_SCALAR_LB ${CPU}"
+PCFLAG="-mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c -DPOLYBENCH_TIME -DPOLYBENCH_USE_SCALAR_LB ${CPU}"
+fi
 
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/2mm/2mm.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o 2mm
+pollycc="${LLVM_INSTALL}/bin/clang -Xclang -load -Xclang ${LLVM_INSTALL}/lib/LLVMPolly.so"
 
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/3mm/3mm.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o 3mm
+${LLVM_INSTALL}/bin/clang ${CFLAG} datamining/covariance/covariance.c -o polybench/covariance
 
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/atax/atax.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o atax
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/2mm/2mm.c -o polybench/2mm
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/3mm/3mm.c -o polybench/3mm
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/atax/atax.c -o polybench/atax
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/bicg/bicg.c -o polybench/bicg
+#${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/cholesky/cholesky.c -o polybench/cholesky
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/doitgen/doitgen.c -o polybench/doitgen
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/gemm/gemm.c -o polybench/gemm
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/gemver/gemver.c -o polybench/gemver
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/gesummv/gesummv.c -o polybench/gesummv
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/mvt/mvt.c -o polybench/mvt
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/symm/symm.c -o polybench/symm
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/syr2k/syr2k.c -o polybench/syr2k
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/syrk/syrk.c -o polybench/syrk
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/trisolv/trisolv.c -o polybench/trisolv
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/kernels/trmm/trmm.c -o polybench/trmm
 
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/bicg/bicg.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o bicg
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/solvers/durbin/durbin.c -o polybench/durbin
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/solvers/dynprog/dynprog.c -o polybench/dynprog
+#${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/solvers/gramschmidt/gramschmidt.c -o polybench/gramschmidt
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/solvers/lu/lu.c -o polybench/lu
+${LLVM_INSTALL}/bin/clang ${CFLAG} linear-algebra/solvers/ludcmp/ludcmp.c -o polybench/ludcmp
 
-#${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/cholesky/cholesky.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o cholesky
+${LLVM_INSTALL}/bin/clang ${CFLAG} medley/floyd-warshall/floyd-warshall.c -o polybench/floyd-warshall
+${LLVM_INSTALL}/bin/clang ${CFLAG} medley/reg_detect/reg_detect.c -o polybench/reg_detect
 
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/doitgen/doitgen.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o doitgen
-
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/gemm/gemm.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o gemm
-
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/gemver/gemver.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o gemver
-
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/gesummv/gesummv.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o gesummv
-
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/mvt/mvt.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o mvt
-
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/symm/symm.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o symm
-
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/syr2k/syr2k.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o syr2k
-
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/syrk/syrk.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o syrk
-
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/trisolv/trisolv.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o trisolv
-
-${LLVM_INSTALL}/bin/clang -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/trmm/trmm.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o trmm
-
-
-
-
-
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c datamining/covariance/covariance.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o covariance.polly
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/2mm/2mm.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o 2mm.polly
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/3mm/3mm.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o 3mm.polly
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/atax/atax.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o atax.polly
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/bicg/bicg.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o bicg.polly
-
-#pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/cholesky/cholesky.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o cholesky
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/doitgen/doitgen.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o doitgen.polly
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/gemm/gemm.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o gemm.polly
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/gemver/gemver.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o gemver.polly
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/gesummv/gesummv.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o gesummv.polly
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/mvt/mvt.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o mvt.polly
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/symm/symm.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o symm
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/syr2k/syr2k.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o syr2k
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/syrk/syrk.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o syrk
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/trisolv/trisolv.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o trisolv
-
-pollycc -mllvm -polly -mllvm -polly-ignore-aliasing -O3 -I utilities -I datamining/covariance utilities/polybench.c linear-algebra/kernels/trmm/trmm.c -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_SCALAR_LB -o trmm
+${LLVM_INSTALL}/bin/clang ${CFLAG} stencils/adi/adi.c -o polybench/adi
+${LLVM_INSTALL}/bin/clang ${CFLAG} stencils/fdtd-2d/fdtd-2d.c -o polybench/fdtd-2d
+${LLVM_INSTALL}/bin/clang ${CFLAG} stencils/fdtd-apml/fdtd-apml.c -o polybench/fdtd-apml
+${LLVM_INSTALL}/bin/clang ${CFLAG} stencils/jacobi-1d-imper/jacobi-1d-imper.c -o polybench/jacobi-1d-imper
+${LLVM_INSTALL}/bin/clang ${CFLAG} stencils/jacobi-2d-imper/jacobi-2d-imper.c -o polybench/jacobi-2d-imper
+${LLVM_INSTALL}/bin/clang ${CFLAG} stencils/seidel-2d/seidel-2d.c -o polybench/seidel-2d
 
 
 
+${pollycc} ${PCFLAG} datamining/covariance/covariance.c -o polybench/covariance.polly
+
+${pollycc} ${PCFLAG} linear-algebra/kernels/2mm/2mm.c -o polybench/2mm.polly
+${pollycc} ${PCFLAG} linear-algebra/kernels/3mm/3mm.c -o polybench/3mm.polly
+${pollycc} ${PCFLAG} linear-algebra/kernels/atax/atax.c -o polybench/atax.polly
+${pollycc} ${PCFLAG} linear-algebra/kernels/bicg/bicg.c -o polybench/bicg.polly
+#${pollycc} ${PCFLAG} linear-algebra/kernels/cholesky/cholesky.c -o polybench/cholesky.polly
+${pollycc} ${PCFLAG} linear-algebra/kernels/doitgen/doitgen.c -o polybench/doitgen.polly
+${pollycc} ${PCFLAG} linear-algebra/kernels/gemm/gemm.c -o polybench/gemm.polly
+${pollycc} ${PCFLAG} linear-algebra/kernels/gemver/gemver.c -o polybench/gemver.polly
+${pollycc} ${PCFLAG} linear-algebra/kernels/gesummv/gesummv.c -o polybench/gesummv.polly
+${pollycc} ${PCFLAG} linear-algebra/kernels/mvt/mvt.c -o polybench/mvt.polly
+${pollycc} ${PCFLAG} linear-algebra/kernels/symm/symm.c -o polybench/symm.polly
+${pollycc} ${PCFLAG} linear-algebra/kernels/syr2k/syr2k.c -o polybench/syr2k.polly
+${pollycc} ${PCFLAG} linear-algebra/kernels/syrk/syrk.c -o polybench/syrk.polly
+${pollycc} ${PCFLAG} linear-algebra/kernels/trisolv/trisolv.c -o polybench/trisolv.polly
+${pollycc} ${PCFLAG} linear-algebra/kernels/trmm/trmm.c -o polybench/trmm.polly
+
+${pollycc} ${PCFLAG} linear-algebra/solvers/durbin/durbin.c -o polybench/durbin.polly
+${pollycc} ${PCFLAG} linear-algebra/solvers/dynprog/dynprog.c -o polybench/dynprog.polly
+#${pollycc} ${PCFLAG} linear-algebra/solvers/gramschmidt/gramschmidt.c -o polybench/gramschmidt.polly
+${pollycc} ${PCFLAG} linear-algebra/solvers/lu/lu.c -o polybench/lu.polly
+${pollycc} ${PCFLAG} linear-algebra/solvers/ludcmp/ludcmp.c -o polybench/ludcmp.polly
+
+${pollycc} ${PCFLAG} medley/floyd-warshall/floyd-warshall.c -o polybench/floyd-warshall.polly
+${pollycc} ${PCFLAG} medley/reg_detect/reg_detect.c -o polybench/reg_detect.polly
+
+${pollycc} ${PCFLAG} stencils/adi/adi.c -o polybench/adi.polly
+${pollycc} ${PCFLAG} stencils/fdtd-2d/fdtd-2d.c -o polybench/fdtd-2d.polly
+${pollycc} ${PCFLAG} stencils/fdtd-apml/fdtd-apml.c -o polybench/fdtd-apml.polly
+${pollycc} ${PCFLAG} stencils/jacobi-1d-imper/jacobi-1d-imper.c -o polybench/jacobi-1d-imper.polly
+${pollycc} ${PCFLAG} stencils/jacobi-2d-imper/jacobi-2d-imper.c -o polybench/jacobi-2d-imper.polly
+${pollycc} ${PCFLAG} stencils/seidel-2d/seidel-2d.c -o polybench/seidel-2d.polly
 
--- docs/BackendTutorial/note/polybench-c-3.2/run.sh
+++ docs/BackendTutorial/note/polybench-c-3.2/run.sh
@@ -1,4 +1,7 @@
-#!/bin/sh -a
+#!/bin/bash
+
+#DIFF=0
+DIFF=1
 
 TIME="time -f \"%E real, %U user, %S sys\""
 #TIME=""
@@ -7,95 +10,239 @@ echo "\ntime ./covariance"
 ./covariance 2> result
 echo "time ./covariance.polly"
 ./covariance.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result;
+fi
+
+
 
 echo "\ntime ./2mm"
 ./2mm 2> result
 echo "time ./2mm.polly"
 ./2mm.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
 
 echo "\ntime ./3mm"
 ./3mm 2> result
 echo "time ./3mm.polly"
 ./3mm.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
 
 echo "\ntime ./atax"
 ./atax 2> result
 echo "time ./atax.polly"
 ./atax.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
 
 echo "\ntime ./bicg"
 ./bicg 2> result
-echo "\ntime ./bicg.polly"
+echo "time ./bicg.polly"
 ./bicg.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
 
 #echo "time ./cholesky"
 #./cholesky 2> result
 #echo "time ./cholesky.polly"
 #./cholesky.polly 2> polly.result
-#diff result polly.result
+#if [ ${DIFF} == 1 ]; then
+#  diff result polly.result
+#fi
 
 echo "\ntime ./doitgen"
 ./doitgen 2> result
 echo "time ./doitgen.polly"
 ./doitgen.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
 
 echo "\ntime ./gemm"
 ./gemm 2> result
 echo "time ./gemm.polly"
 ./gemm.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
 
 echo "\ntime ./gemver"
 ./gemver 2> result
 echo "time ./gemver.polly"
 ./gemver.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
 
 echo "\ntime ./gesummv"
 ./gesummv 2> result
 echo "time ./gesummv.polly"
 ./gesummv.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
 
 echo "\ntime ./mvt"
 ./mvt 2> result
 echo "time ./mvt.polly"
 ./mvt.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
 
 echo "\ntime ./symm"
 ./symm 2> result
 echo "time ./symm.polly"
 ./symm.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
 
 echo "\ntime ./syr2k"
 ./syr2k 2> result
 echo "time ./syr2k.polly"
 ./syr2k.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
 
 echo "\ntime ./syrk"
 ./syrk 2> result
 echo "time ./syrk.polly"
 ./syrk.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
 
 echo "\ntime ./trisolv"
 ./trisolv 2> result
 echo "time ./trisolv.polly"
 ./trisolv.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
 
 echo "\ntime ./trmm"
 ./trmm 2> result
 echo "time ./trmm.polly"
 ./trmm.polly 2> polly.result
-diff result polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
+
+
+
+echo "\ntime ./durbin"
+./durbin 2> result
+echo "time ./durbin.polly"
+./durbin.polly 2> polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
+
+echo "\ntime ./dynprog"
+./dynprog 2> result
+echo "time ./dynprog.polly"
+./dynprog.polly 2> polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
+
+#echo "\ntime ./gramschmidt"
+#./gramschmidt 2> result
+#echo "time ./gramschmidt.polly"
+#./gramschmidt.polly 2> polly.result
+#if [ ${DIFF} == 1 ]; then
+#  diff result polly.result
+#fi
+
+echo "\ntime ./lu"
+./lu 2> result
+echo "time ./lu.polly"
+./lu.polly 2> polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
+
+echo "\ntime ./ludcmp"
+./ludcmp 2> result
+echo "time ./ludcmp.polly"
+./ludcmp.polly 2> polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
+
+
+
+echo "\ntime ./floyd-warshall"
+./floyd-warshall 2> result
+echo "time ./floyd-warshall.polly"
+./floyd-warshall.polly 2> polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
+
+echo "\ntime ./reg_detect"
+./reg_detect 2> result
+echo "time ./reg_detect.polly"
+./reg_detect.polly 2> polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
+
+
+
+echo "\ntime ./adi"
+./adi 2> result
+echo "time ./adi.polly"
+./adi.polly 2> polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
+
+echo "\ntime ./fdtd-2d"
+./fdtd-2d 2> result
+echo "time ./fdtd-2d.polly"
+./fdtd-2d.polly 2> polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
+
+echo "\ntime ./fdtd-apml"
+./fdtd-apml 2> result
+echo "time ./fdtd-apml.polly"
+./fdtd-apml.polly 2> polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
+
+echo "\ntime ./jacobi-1d-imper"
+./jacobi-1d-imper 2> result
+echo "time ./jacobi-1d-imper.polly"
+./jacobi-1d-imper.polly 2> polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
+
+echo "\ntime ./jacobi-2d-imper"
+./jacobi-2d-imper 2> result
+echo "time ./jacobi-2d-imper.polly"
+./jacobi-2d-imper.polly 2> polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
+
+echo "\ntime ./seidel-2d"
+./seidel-2d 2> result
+echo "time ./seidel-2d.polly"
+./seidel-2d.polly 2> polly.result
+if [ ${DIFF} == 1 ]; then
+  diff result polly.result
+fi
 
